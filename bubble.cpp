25b32dec5a41475a1040cbdb391c01a49f0000c9
ABHISHEK PATEL <69916475+Abhishek10patel@users.noreply.github.com>
2021-10-08 16:37:26 +0530

    Update bubble.cpp

--- bubble.cpp
+++ bubble.cpp
@@ -33,7 +33,7 @@ void printArray(int arr[], int size)
 // Driver code
 int main()
 {
-	int arr[] = {64, 34, 25, 12, 22, 11, 90};
+	int arr[] = {64, 34, 25, 12, 22,  69 , 95, 11, 90};
 	int n = sizeof(arr)/sizeof(arr[0]);
 	bubbleSort(arr, n);
 	cout<<"Sorted array: \n";
